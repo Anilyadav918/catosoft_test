c1dc6bb80a47fc33360a521cc9e3d37126eb60fb
Anil yadav <anilyadav918@gmail.com>
2020-06-10 23:31:51 +0530

    final commit

--- __test__/servers.test.js
+++ __test__/servers.test.js
@@ -1,18 +1,18 @@
-// const { getServers } = require('../controllers/servers');
-// const supertest = require('supertest');
-//const app = require('../app');
-/*
-test('should output url with priority', async() => {
-    const url = await getServers(app).get('/');
+//  const { getServers } = require('../controllers/servers');
+//  const supertest = require('supertest');
+// const app = require('../app');
 
-    expect(res.status).toBe(200);
-    expect(res.status(200).json(result)).toBe([
-        {
-            "url": "http://instagram.com",
-            "priority": 3
-        }
-    ])
-})*/
+// test('should output url with priority', async() => {
+//     const url = await getServers(app).get('/api/v1/servers');
+
+//     expect(res.status).toBe(200);
+//     expect(res.status(200).json(result)).toBe([
+//         {
+//             "url": "http://instagram.com",
+//             "priority": 3
+//         }
+//     ])
+// })
 
 
 // describe("Testing servers api", () => {
@@ -29,15 +29,22 @@ const chaiHttp = require("chai-http");
 const expect = chai.expect
 const base_url = "http://localhost:5000";
 chai.use(chaiHttp);
-describe("First Test", function(){
-    it('get all servers', function(done){
+describe("First Test", ()=>{
+    it('get all servers', (done)=>{
         chai.request(base_url)
             .get('/api/v1/servers')
-            .end(function(err, res){
+            .end((err, res)=>{
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.an("array");
+                const result = res.body(server => {
+                    return {
+                        "url" : server.url,
+                        "priority": server.priority
+                    }
+                })
                 expect(res.body.url).to.equal(result.url);
                 expect(res.body.priority).to.equal(result.priority);
+                done();
             })
     })
 })
--- controllers/servers.js
+++ controllers/servers.js
@@ -1,41 +1,54 @@
+
 const asyncHandler = require('../middleware/async');
-const Servers = require('../models/Servers.json');
+const Servers = require('../models/Servers');
 
 //Function to get all servers async/await
-exports.getServers = asyncHandler(async(req, res, next) => {
+    exports.getServers = asyncHandler(async(req, res, next) => {
         const servers = await Servers;
         let url;
         let priority;
         let flag = false;
-        let result = servers.map(server => {
-            url = server.url;
-            let data;
-            priority = server.priority;
-            let url_ = server.url;
+        let result = servers.filter(server => {
+        url = server.url;
+        let data;
+        priority = server.priority;
+        //taking out the url from the server array
+        let url_ = server.url;
+        //splitting th array by // so that it becomes [ 'http:', 'doesNotExist.boldtech.co' ]
+        url_ = url_.split('//');
+        //again splitting the second part of the above url_ array to get in form [ 'doesNotExist', 'boldtech', 'co' ]
+        url_ = url_[1].split('.');
+        //now taking the first part of the above array and converting it to lower case for comparison
+        url_ = url_[0].toLowerCase();
+        //Checking for offline users 
+        return url_ !== "offline" && url_ !== "doesnotexist";
             
-            url_ = url_.split('//');
-             
-            url_ = url_[1].split('.');
-            
-            url_ = url_[0].toLowerCase();
-                
-                if(url_ === 'offline' || url_ === 'doesnotexist'){
-                    flag = false;
-                    //return ;
-                }else if(url_ !== 'offline' || url_ !== 'doesnotexist'){
-                    flag = true;
-                    data = {url, priority};
-                    return data;      
-             }
-             return data;
+        // if(url_ !== "offline" && url_ !== "doesnotexist"){
+        //     flag = true;
+        //     data = {url, priority};      
+        // } else {
+        //     return {};
+        // }
+        // return data;
+    })
+        //
+        if (result.length > 0) {
+        
+        result = result.map((server) => {
+            return {
+                "url" : server.url,
+                "priority": server.priority
+            }
+
         })
-    
-            //sorting the result on their priorities in ascending order
-            result.sort((a,b) => {
-                return a.priority - b.priority;
-            })
-            //sending response
-            res.status(200).json(result);
 
+        //sorting the result on their priorities in ascending order
+        result.sort((a,b) => {
+            return a.priority - b.priority;
+        })
+        //sending response
+        res.status(200).json(result);
+        } else {
+            res.status(400).send("All servers are offline");
+        }
 })
-
